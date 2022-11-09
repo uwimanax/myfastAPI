4f01a474a49b573760680fc5e43bee5b2353fd76
uwimanax <uwimanaxavi10@gmail.com>
2022-11-09 12:50:53 +0200

    created our first API with index, today mynames

---
+++ api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, Request
+from datetime import datetime
+storage = FastAPI(title ='MY FASTAPI')
+
+@storage.get('/')
+def index():
+    return "this is the index/first page of my API"
+#if __name__== '__main__':
+   # app.run()    
+
+@storage.get('/today')
+def today():
+    return str(datetime.now())
+@storage.get('/mynames')
+def names(first_name:bool=False, last_name:bool=False, Full_name:bool=False):
+    full_names = ""
+    if first_name:
+        full_names += 'Xaverine'
+    if last_name:
+        full_names += ' UWIMANA' 
+    if Full_name:
+        full_names += "my name is xaverine UWIMANA"  
+    return full_names     
